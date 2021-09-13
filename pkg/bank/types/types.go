edb215711f9cd3a57ec6fc7f452e3c74275e5774
Behzod <behzod.b01@gmail.com>
2021-09-13 20:45:38 +0500

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,14 @@
+package types
+
+//Money  представляет собой денежную сумму в минимальных единицах(центы, копейки, и т.д)
+type Money int64
+
+//Category  представляет сабой категорию в которой был совершен платёж(авто, рестораны и т.д)
+type Category string
+
+//Payment представляет  информацию о платеже  
+type Payment struct {
+	ID int
+	Amount Money
+	Category Category
+}
